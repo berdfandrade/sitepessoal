c885a6bfd409b07c32aca22adc2729acef071c3a
Bernardo Andrade <berdfandrade@gmail.com>
2023-09-18 21:57:54 -0300

    Escrevendo as experiências que tive

---
+++ src/componentes/PaginaExperiencia/Experiencia.js
@@ -0,0 +1,42 @@
+
+
+/*
+
+  Objeto que contém as 
+  informações sobre a 
+  experiência
+
+*/
+
+
+const experiencia = [
+  {
+    empresa: "CEIS ",
+    tempo: "2017-2018",
+    texto:
+      "Criação de uma rede social interna para os estudantes de comunicação. A aplicação foi feita usando o site do laboratório em um domínio da PUC - Minas. A pequena rede social foi desenvolvida inteiramente por mim através de alguns plugins simples do WordPress, e através de um design elaborado por mim em conjunto com o NEP (Núcleo de Experimentação Publicitária).",
+  },
+
+  {
+    empresa: "Freelancer audiovisual ",
+    tempo: "2020-2023",
+    texto:
+      "Escrevi roteiros para projetos audiovisuais, editei e gravei videos exercendo a minha formação primária que é Cinema e Audiovisual.",
+  },
+
+  {
+    empresa: "Orquesta Filarmônica de Minas Gerais ",
+    tempo: "2020-2023",
+    texto:
+      "Trabalhei como a transmissão de concertos ao vivo da Sala Minas Gerais, tansmitindo a Orquestra Filarmônica. Também trabalhei como assistente na elaboração do nosso fluxo de trabalho interno involvendo redes internas. Atuei também trabalhando em parte na manutenção do site da Orquestra em HTML simples.",
+  },
+  {
+    empresa: "h3aven ",
+    tempo: "2023-atualmente",
+    texto:
+      "Atuo na parte de criação front-end de alguns produtos da empresa. Também faço algumas funções como programador full-stack. Atualmente utilizo React, TypeScript, Node.Js e Prisma no trabalho. Apesar a equipe usar Solidity, não possuo experiência com a ferramenta. ",
+  },
+
+];
+
+export default experiencia;
